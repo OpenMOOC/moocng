80c0dc8748dfcda0e8c9497c1d8fc45a7ad35bbf
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-07-30 11:07:01 +0200

    Mount the courses app at the root

--- moocng/urls.py
+++ moocng/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns(
     '',
     # Examples:
     # url(r'^$', 'moocng.views.home', name='home'),
-    url(r'^moocng/', include('moocng.courses.urls')),
+    url(r'^$', include('moocng.courses.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
