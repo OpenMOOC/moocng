0961e6106a2d12e226e251c5e366c24699b3cd07
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-07-30 10:16:17 +0200

    Add support for local_settings and sane defaults for the database connection

--- moocng/settings.py
+++ moocng/settings.py
@@ -12,9 +12,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'NAME': 'moocng',                # Or path to database file if using sqlite3.
+        'USER': 'moocng',                # Not used with sqlite3.
+        'PASSWORD': 'moocng',            # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -152,3 +152,8 @@ LOGGING = {
         },
     }
 }
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
