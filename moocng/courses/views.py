24f707ea4139a7b6002392c4b61cfe7c322dcd34
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-08-01 01:04:27 +0200

    Course enrollment

--- moocng/courses/views.py
+++ moocng/courses/views.py
@@ -1,6 +1,10 @@
+from django.contrib.messages import success
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import (get_object_or_404, get_list_or_404,
                               render_to_response)
 from django.template import RequestContext
+from django.utils.translation import ugettext as _
 
 from moocng.courses.models import Course, Unit
 
@@ -14,8 +18,23 @@ def home(request):
 def course_overview(request, course_slug):
     course = get_object_or_404(Course, slug=course_slug)
 
+    if request.user.is_authenticated():
+        is_enrolled = course.students.filter(id=request.user.id).exists()
+    else:
+        is_enrolled = False
+
+    if request.method == 'POST':
+        course.students.add(request.user)
+        course.save()
+        success(request,
+                _(u'Congratulations, you have successfully enroll in the course %(course)s')
+                % {'course': unicode(course)})
+        return HttpResponseRedirect(reverse('course_overview',
+                                            args=(course.slug, )))
+
     return render_to_response('courses/overview.html', {
             'course': course,
+            'is_enrolled': is_enrolled
             }, context_instance=RequestContext(request))
 
 
