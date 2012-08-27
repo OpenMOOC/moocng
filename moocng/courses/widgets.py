838e02d8ca53b9307fdc9975614949acd43daba5
Alejandro Blanco <ablanco@yaco.es>
2012-08-27 09:54:19 +0200

    Bug fix loading options graphical editor

--- moocng/courses/widgets.py
+++ moocng/courses/widgets.py
@@ -4,4 +4,7 @@ from django.utils.safestring import mark_safe
 class ImageReadOnlyWidget(Widget):
 
     def render(self, name, value, attrs=None):
-        return mark_safe(u'<img src="%s" />' % value.url)
+        if value is not None:
+            return mark_safe(u'<img src="%s" />' % value.url)
+        else:
+            return u''
