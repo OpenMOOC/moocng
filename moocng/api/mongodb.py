fcb0626f4371ccc42649d4dda3ee8187ea828789
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-08-29 16:53:27 +0200

    Use a more robust database wrapper

--- moocng/api/mongodb.py
+++ moocng/api/mongodb.py
@@ -1,5 +1,3 @@
-import bson
-
 import urlparse
 
 from django.conf import settings
@@ -11,23 +9,39 @@ from tastypie.bundle import Bundle
 from tastypie.exceptions import NotFound
 from tastypie.resources import Resource
 
+DEFAULT_MONGODB_HOST = 'localhost'
+DEFAULT_MONGODB_PORT = 27017
+DEFAULT_MONGODB_NAME = 'moocng'
+DEFAULT_MONGODB_URI = 'mongodb://%s:%d/%s' % (DEFAULT_MONGODB_HOST,
+                                              DEFAULT_MONGODB_PORT,
+                                              DEFAULT_MONGODB_NAME)
+
 
 class MongoDB(object):
 
-    def __init__(self, db_uri):
+    def __init__(self, db_uri=DEFAULT_MONGODB_URI,
+                 connection_factory=Connection):
+        self.db_uri = urlparse.urlparse(db_uri)
+        self.connection = connection_factory(
+            host=self.db_uri.hostname or DEFAULT_MONGODB_HOST,
+            port=self.db_uri.port or DEFAULT_MONGODB_PORT)
 
-        uri_parts = urlparse.urlparse(db_uri)
-
-        if uri_parts.path:
-            database_name = uri_parts.path[1:]
+        if self.db_uri.path:
+            self.database_name = self.db_uri.path[1:]
         else:
-            try:
-                database_name = settings.MONGODB_NAME
-            except AttributeError:
-                raise ImproperlyConfigured('You did not supply the database name in MONGODB_URI neither in MONGODB_NAME')
+            self.database_name = DEFAULT_MONGODB_NAME
 
-        self.connection = Connection(db_uri)
-        self.database = self.connection[database_name]
+        self.database = self.get_database()
+
+    def get_connection(self):
+        return self.connection
+
+    def get_database(self):
+        database = self.connection[self.database_name]
+        if self.db_uri.username and self.db_uri.password:
+            database.authenticate(self.db_uri.username, self.db_uri.password)
+
+        return database
 
     def get_collection(self, collection):
         return self.database[collection]
