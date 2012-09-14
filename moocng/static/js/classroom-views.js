e04f80d492834889086854449c08043f6e7783a1
Antonio Pérez-Aranda Alcaide <ant30tx@gmail.com>
2012-09-14 11:11:01 +0200

    Remove youtube related videos when video is finished

--- moocng/static/js/classroom-views.js
+++ moocng/static/js/classroom-views.js
@@ -83,6 +83,7 @@ MOOC.views.KnowledgeQuantum = Backbone.View.extend({
 
         html = '<iframe id="ytplayer" width="100%" height="' + height + 'px" ';
         html += 'src="http://www.youtube.com/embed/' + this.model.get("videoID");
+        html += '?rel=0';
         html += '" frameborder="0" allowfullscreen></iframe>';
         $("#kq-video").html(html);
 
@@ -357,6 +358,7 @@ MOOC.views.KnowledgeQuantum = Backbone.View.extend({
             if (!_.isNull(this.model.get("questionInstance").get("solution"))) {
                 html = '<iframe id="ytplayer" width="100%" height="' + height + 'px" ';
                 html += 'src="http://www.youtube.com/embed/' + this.model.get("questionInstance").get("solution");
+                html += '?rel=0';
                 html += '" frameborder="0" allowfullscreen></iframe>';
             } else {
                 MOOC.alerts.show(MOOC.alerts.INFO, MOOC.trans.api.solutionNotReadyTitle, MOOC.trans.api.solutionNotReady);
