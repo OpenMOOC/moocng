550c0445e5763354df6fbcac611dc5469014e13c
Sixto Martin <smartin@yaco.es>
2012-12-18 14:00:32 +0100

    Fix problem related to the teacheradmin form, exclude owner

--- moocng/teacheradmin/forms.py
+++ moocng/teacheradmin/forms.py
@@ -22,7 +22,7 @@ class CourseForm(ModelForm):
 
     class Meta:
         model = Course
-        exclude = ('slug', 'teachers',)
+        exclude = ('slug', 'teachers', 'owner',)
 
     def get_pretty_errors(self):
         errors = ErrorDict()
