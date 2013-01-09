278c6e6e0f3a72c08bdffd964886b33499023506
Sixto Martin <smartin@yaco.es>
2013-01-09 12:05:48 +0100

    Improve search of users in the admin interface

--- moocng/courses/admin.py
+++ moocng/courses/admin.py
@@ -46,7 +46,7 @@ logger = logging.getLogger(__name__)
 class CourseAdmin(SortableAdmin):
 
     prepopulated_fields = {'slug': ('name', )}
-
+    filter_horizontal = ('teachers', 'students')
 
 class AnnouncementAdmin(admin.ModelAdmin):
 
