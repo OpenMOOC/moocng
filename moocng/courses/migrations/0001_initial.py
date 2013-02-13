ee3dd3f33e680d299aaf70afa22a684b5265fbc0
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2013-02-13 17:27:58 +0100

    Add South support to the courses application

---
+++ moocng/courses/migrations/0001_initial.py
@@ -0,0 +1,278 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Course'
+        db.create_table('courses_course', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('order', self.gf('django.db.models.fields.PositiveIntegerField')(default=1, db_index=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50)),
+            ('description', self.gf('tinymce.models.HTMLField')()),
+            ('requirements', self.gf('tinymce.models.HTMLField')(blank=True)),
+            ('intended_audience', self.gf('tinymce.models.HTMLField')(null=True, blank=True)),
+            ('estimated_effort', self.gf('tinymce.models.HTMLField')(null=True, blank=True)),
+            ('learning_goals', self.gf('tinymce.models.HTMLField')(blank=True)),
+            ('start_date', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
+            ('end_date', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
+            ('owner', self.gf('django.db.models.fields.related.ForeignKey')(related_name='courses_as_owner', to=orm['auth.User'])),
+            ('promotion_video', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
+            ('threshold', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=4, decimal_places=2, blank=True)),
+            ('certification_available', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('certification_banner', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
+            ('completion_badge', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='course', unique=True, null=True, to=orm['badges.Badge'])),
+            ('enrollment_method', self.gf('django.db.models.fields.CharField')(default='free', max_length=200)),
+        ))
+        db.send_create_signal('courses', ['Course'])
+
+        # Adding M2M table for field teachers on 'Course'
+        db.create_table('courses_course_teachers', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('course', models.ForeignKey(orm['courses.course'], null=False)),
+            ('user', models.ForeignKey(orm['auth.user'], null=False))
+        ))
+        db.create_unique('courses_course_teachers', ['course_id', 'user_id'])
+
+        # Adding M2M table for field students on 'Course'
+        db.create_table('courses_course_students', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('course', models.ForeignKey(orm['courses.course'], null=False)),
+            ('user', models.ForeignKey(orm['auth.user'], null=False))
+        ))
+        db.create_unique('courses_course_students', ['course_id', 'user_id'])
+
+        # Adding model 'Announcement'
+        db.create_table('courses_announcement', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50)),
+            ('content', self.gf('tinymce.models.HTMLField')()),
+            ('course', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['courses.Course'])),
+            ('datetime', self.gf('django.db.models.fields.DateTimeField')()),
+        ))
+        db.send_create_signal('courses', ['Announcement'])
+
+        # Adding model 'Unit'
+        db.create_table('courses_unit', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('order', self.gf('django.db.models.fields.PositiveIntegerField')(default=1, db_index=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('course', self.gf('adminsortable.fields.SortableForeignKey')(to=orm['courses.Course'])),
+            ('unittype', self.gf('django.db.models.fields.CharField')(default='n', max_length=1)),
+            ('start', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('deadline', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('weight', self.gf('django.db.models.fields.SmallIntegerField')(default=0)),
+        ))
+        db.send_create_signal('courses', ['Unit'])
+
+        # Adding model 'KnowledgeQuantum'
+        db.create_table('courses_knowledgequantum', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('order', self.gf('django.db.models.fields.PositiveIntegerField')(default=1, db_index=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('unit', self.gf('adminsortable.fields.SortableForeignKey')(to=orm['courses.Unit'])),
+            ('weight', self.gf('django.db.models.fields.SmallIntegerField')(default=0)),
+            ('video', self.gf('django.db.models.fields.URLField')(max_length=200)),
+            ('teacher_comments', self.gf('tinymce.models.HTMLField')(blank=True)),
+            ('supplementary_material', self.gf('tinymce.models.HTMLField')(blank=True)),
+        ))
+        db.send_create_signal('courses', ['KnowledgeQuantum'])
+
+        # Adding model 'Attachment'
+        db.create_table('courses_attachment', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('kq', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['courses.KnowledgeQuantum'])),
+            ('attachment', self.gf('django.db.models.fields.files.FileField')(max_length=100)),
+        ))
+        db.send_create_signal('courses', ['Attachment'])
+
+        # Adding model 'Question'
+        db.create_table('courses_question', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('kq', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['courses.KnowledgeQuantum'], unique=True)),
+            ('solution', self.gf('django.db.models.fields.URLField')(max_length=200)),
+            ('last_frame', self.gf('django.db.models.fields.files.ImageField')(max_length=100, blank=True)),
+            ('use_last_frame', self.gf('django.db.models.fields.BooleanField')(default=True)),
+        ))
+        db.send_create_signal('courses', ['Question'])
+
+        # Adding model 'Option'
+        db.create_table('courses_option', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('question', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['courses.Question'])),
+            ('x', self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=0)),
+            ('y', self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=0)),
+            ('width', self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=100)),
+            ('height', self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=12)),
+            ('optiontype', self.gf('django.db.models.fields.CharField')(default='t', max_length=1)),
+            ('solution', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('text', self.gf('django.db.models.fields.CharField')(max_length=500, null=True, blank=True)),
+        ))
+        db.send_create_signal('courses', ['Option'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Course'
+        db.delete_table('courses_course')
+
+        # Removing M2M table for field teachers on 'Course'
+        db.delete_table('courses_course_teachers')
+
+        # Removing M2M table for field students on 'Course'
+        db.delete_table('courses_course_students')
+
+        # Deleting model 'Announcement'
+        db.delete_table('courses_announcement')
+
+        # Deleting model 'Unit'
+        db.delete_table('courses_unit')
+
+        # Deleting model 'KnowledgeQuantum'
+        db.delete_table('courses_knowledgequantum')
+
+        # Deleting model 'Attachment'
+        db.delete_table('courses_attachment')
+
+        # Deleting model 'Question'
+        db.delete_table('courses_question')
+
+        # Deleting model 'Option'
+        db.delete_table('courses_option')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'badges.badge': {
+            'Meta': {'ordering': "['-modified', '-created']", 'object_name': 'Badge'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'}),
+            'title': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'courses.announcement': {
+            'Meta': {'object_name': 'Announcement'},
+            'content': ('tinymce.models.HTMLField', [], {}),
+            'course': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['courses.Course']"}),
+            'datetime': ('django.db.models.fields.DateTimeField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'courses.attachment': {
+            'Meta': {'object_name': 'Attachment'},
+            'attachment': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'kq': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['courses.KnowledgeQuantum']"})
+        },
+        'courses.course': {
+            'Meta': {'ordering': "['order']", 'object_name': 'Course'},
+            'certification_available': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'certification_banner': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'completion_badge': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'course'", 'unique': 'True', 'null': 'True', 'to': "orm['badges.Badge']"}),
+            'description': ('tinymce.models.HTMLField', [], {}),
+            'end_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'enrollment_method': ('django.db.models.fields.CharField', [], {'default': "'free'", 'max_length': '200'}),
+            'estimated_effort': ('tinymce.models.HTMLField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'intended_audience': ('tinymce.models.HTMLField', [], {'null': 'True', 'blank': 'True'}),
+            'learning_goals': ('tinymce.models.HTMLField', [], {'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1', 'db_index': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'courses_as_owner'", 'to': "orm['auth.User']"}),
+            'promotion_video': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'requirements': ('tinymce.models.HTMLField', [], {'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'start_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'students': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'courses_as_student'", 'blank': 'True', 'to': "orm['auth.User']"}),
+            'teachers': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'courses_as_teacher'", 'symmetrical': 'False', 'to': "orm['auth.User']"}),
+            'threshold': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '4', 'decimal_places': '2', 'blank': 'True'})
+        },
+        'courses.knowledgequantum': {
+            'Meta': {'ordering': "['order']", 'object_name': 'KnowledgeQuantum'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1', 'db_index': 'True'}),
+            'supplementary_material': ('tinymce.models.HTMLField', [], {'blank': 'True'}),
+            'teacher_comments': ('tinymce.models.HTMLField', [], {'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'unit': ('adminsortable.fields.SortableForeignKey', [], {'to': "orm['courses.Unit']"}),
+            'video': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'weight': ('django.db.models.fields.SmallIntegerField', [], {'default': '0'})
+        },
+        'courses.option': {
+            'Meta': {'object_name': 'Option'},
+            'height': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '12'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'optiontype': ('django.db.models.fields.CharField', [], {'default': "'t'", 'max_length': '1'}),
+            'question': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['courses.Question']"}),
+            'solution': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'text': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'}),
+            'width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '100'}),
+            'x': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '0'}),
+            'y': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '0'})
+        },
+        'courses.question': {
+            'Meta': {'object_name': 'Question'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'kq': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['courses.KnowledgeQuantum']", 'unique': 'True'}),
+            'last_frame': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
+            'solution': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'use_last_frame': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
+        },
+        'courses.unit': {
+            'Meta': {'ordering': "['order']", 'object_name': 'Unit'},
+            'course': ('adminsortable.fields.SortableForeignKey', [], {'to': "orm['courses.Course']"}),
+            'deadline': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1', 'db_index': 'True'}),
+            'start': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'unittype': ('django.db.models.fields.CharField', [], {'default': "'n'", 'max_length': '1'}),
+            'weight': ('django.db.models.fields.SmallIntegerField', [], {'default': '0'})
+        }
+    }
+
+    complete_apps = ['courses']
