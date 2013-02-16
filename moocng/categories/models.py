61f6ac5f3276a9517aabd3908651fe9ddb9d15ca
Alejandro Blanco <alejandro.b.e@gmail.com>
2013-02-16 21:12:57 +0100

    Spanish categories-related translations

--- moocng/categories/models.py
+++ moocng/categories/models.py
@@ -9,9 +9,10 @@ class Category(models.Model):
     name = models.CharField(verbose_name=_(u'Name'), max_length=200,
                             blank=False, null=False)
     slug = models.SlugField(verbose_name=_(u'Slug'), blank=False, null=False)
-    icon = models.ImageField(verbose_name=_(u'Category icon'),
+    icon = models.ImageField(verbose_name=_(u'Category\'s icon'),
                              upload_to='category_icons', blank=True, null=True)
     courses = models.ManyToManyField(Course, related_name='categories',
+                                     verbose_name=_(u'Courses'),
                                      blank=True, null=True)
 
     class Meta:
