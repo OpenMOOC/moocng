c5481ecce249a73768f6db93f6dfc6577cb6bb0f
Yamila Moreno <yamila.moreno@kaleidos.net>
2013-03-05 10:56:31 +0100

    Create models and initial migration for peer review

--- moocng/peerreview/models.py
+++ moocng/peerreview/models.py
@@ -12,6 +12,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from adminsortable.models import Sortable
+from tinymce.models import HTMLField
+
+from moocng.courses.models import KnowledgeQuantum
+
+
+class PeerReviewAssignment(models.Model):
+    description = HTMLField(verbose_name=_(u'Description'),
+                            blank=True, null=False)
+    minimum_reviewers = models.PositiveSmallIntegerField()
+    knowledge_quantum = models.ForeignKey(KnowledgeQuantum,
+                                          verbose_name=_(u'Knowledge quantum'),
+                                          blank=False, null=False)
+
+    class Meta:
+        verbose_name = _(u'peer review assignment')
+        verbose_name_plural = _(u'peer review assignments')
+
+
+class EvaluationCriterion(Sortable):
+    assignment = models.ForeignKey(PeerReviewAssignment,
+                                   verbose_name=_(u'Peer review assignment'))
+    description = models.TextField(verbose_name=_(u'Description'),
+                                   blank=True, null=False)
+
+    class Meta(Sortable.Meta):
+        verbose_name = _(u'evaluation criterion')
+        verbose_name_plural = _(u'evaluation criteria')
