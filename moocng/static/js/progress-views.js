018d6a341b2f7411d76f7f8e321cb3537539c08f
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2013-03-05 17:03:28 +0100

    Small refactor to move some code from the Unit View to the KQ View

--- moocng/static/js/progress-views.js
+++ moocng/static/js/progress-views.js
@@ -27,8 +27,21 @@ MOOC.views.capitalize = function (text) {
 };
 
 MOOC.views.Unit = Backbone.View.extend({
-    events: {
-        "click": "showUnit"
+
+    initialize: function () {
+        "use strict";
+        var collection = this.model.get('knowledgeQuantumList');
+        this._kqViews = collection.map(function (kq) {
+            var kqView = MOOC.views.kqViews[kq.id];
+            if (_.isUndefined(kqView)) {
+                MOOC.views.kqViews[kq.id] = new MOOC.views.KnowledgeQuantum({
+                    model: kq,
+                    id: "kq" + kq.id
+                });
+                kqView = MOOC.views.kqViews[kq.id];
+            }
+            return kqView;
+        });
     },
 
     render: function () {
@@ -45,51 +58,49 @@ MOOC.views.Unit = Backbone.View.extend({
         html = '<div class="progress progress-info" title="' + MOOC.trans.progress.completed + '"><div class="bar completed" style="width: 0%;"></div></div>';
         html += '<div class="progress progress-success" title="' + MOOC.trans.progress.correct + '"><div class="bar correct" style="width: 0%;"></div></div>';
         $("#unit-progress-bar").html(html);
+
         progress = this.model.calculateProgress({ completed: true });
         helpText = "<div><span>" + Math.round(progress) + "% " + MOOC.trans.progress.completed + "</span></div>";
         $("#unit-progress-bar").find("div.bar.completed").css("width", progress + "%");
+
         progress = this.model.calculateProgress({ completed: true, correct: true });
         helpText += "<div><span>" + Math.round(progress) + "% " + MOOC.trans.progress.correct + "</span></div>";
         $("#unit-progress-bar").find("div.bar.correct").css("width", progress + "%");
+
         $("#unit-progress-text").html(helpText);
 
-        if (this.model.get("knowledgeQuantumList").length === 0) {
-            MOOC.alerts.show(MOOC.alerts.INFO, MOOC.trans.api.unitNotReadyTitle, MOOC.trans.api.unitNotReady);
-            $("#unit-kqs").html("");
-        } else {
-            html = "";
-            this.model.get("knowledgeQuantumList").each(function (kq) {
-                html += "<li title='" + kq.get("title") + "'><b>" + kq.truncateTitle(40) + "</b>";
-                if (kq.get("completed")) {
-                    if (kq.get("correct")) {
-                        html += '<span class="badge badge-success pull-right"><i class="icon-ok icon-white"></i> ' + MOOC.views.capitalize(MOOC.trans.progress.correct) + '</span>';
-                    } else {
-                        html += '<span class="badge badge-important pull-right"><i class="icon-remove icon-white"></i> ' + MOOC.trans.progress.incorrect + '</span>';
-                    }
-                } else {
-                    html += '<span class="badge pull-right">' + MOOC.trans.progress.pending + '</span>';
-                }
-                html += "</li>";
-            });
-            $("#unit-kqs").html(html);
-        }
+
+        $("#unit-kqs").empty();
+        _(this._kqViews).each(function (kqView) {
+            $("#unit-kqs").append(kqView.render().el);
+        });
 
         return this;
-    },
-
-    showUnit: function (evt) {
-        "use strict";
-        if (!this.$el.hasClass("active")) {
-            this.render();
-        }
     }
 });
 
 MOOC.views.unitViews = {};
 
 MOOC.views.KnowledgeQuantum = Backbone.View.extend({
+    tagName: "li",
+
     render: function () {
         "use strict";
+        var html = [
+                "<b>" + this.model.truncateTitle(40) + "</b>"
+            ];
+        if (this.model.get('completed')) {
+            if (this.model.get("correct")) {
+                html.push('<span class="badge badge-success pull-right"><i class="icon-ok icon-white"></i> ' + MOOC.views.capitalize(MOOC.trans.progress.correct) + '</span>');
+            } else {
+                html.push('<span class="badge badge-important pull-right"><i class="icon-remove icon-white"></i> ' + MOOC.trans.progress.incorrect + '</span>');
+            }
+        } else {
+            html.push('<span class="badge pull-right">' + MOOC.trans.progress.pending + '</span>');
+        }
+
+        this.$el.attr("title", this.model.get('title'));
+        this.$el.html(html.join(""));
         return this;
     }
 });
