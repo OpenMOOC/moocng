db6239a9fb2b2b111f1b4060868fe642054caadc
Sixto Martin <smartin@yaco.es>
2013-03-05 13:51:49 +0100

    rename EvaluationCriterion resource

--- moocng/api/urls.py
+++ moocng/api/urls.py
@@ -31,7 +31,7 @@ v1_api.register(resources.ActivityResource())
 v1_api.register(resources.CourseResource())
 v1_api.register(resources.UserResource())
 v1_api.register(resources.PeerReviewAssignmentResource())
-v1_api.register(resources.EvaluationCriterion())
+v1_api.register(resources.EvaluationCriterionResource())
 
 urlpatterns = patterns(
     '',
