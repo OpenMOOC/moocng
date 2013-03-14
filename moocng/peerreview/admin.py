85eca8107ae206110c20874de9760adfac886c5f
Yamila Moreno <yamila.ms@gmail.com>
2013-03-14 17:08:09 +0100

    Added filter to PeerReviewAssignment to Admin page

--- moocng/peerreview/admin.py
+++ moocng/peerreview/admin.py
@@ -26,12 +26,14 @@ logger = logging.getLogger(__name__)
 class PeerReviewAssignmentAdmin(admin.ModelAdmin):
 
     list_display = ('__unicode__', 'kq', 'minimum_reviewers')
+    list_filter = ('kq__unit__course',)
 
 
 class EvaluationCriterionAdmin(SortableAdmin):
 
     list_display = ('title', 'assignment')
     list_filter =  ('assignment', 'assignment__kq__unit__course',)
+
 
 admin.site.register(PeerReviewAssignment, PeerReviewAssignmentAdmin)
 admin.site.register(EvaluationCriterion, EvaluationCriterionAdmin)
