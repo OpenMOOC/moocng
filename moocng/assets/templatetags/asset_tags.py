2e9e2f021b100553d63a861b426229a3ebec9972
José Millán Soto <jmillan@opentia.com>
2013-04-05 20:28:26 +0200

    Created a new template tag to detect if a course has available assets
    A template tag to detect if a course has available assets has been created.
    File assets/utils.py was created with helper functions course_has_assets and
    course_get_assets.
    
    Base course template has been modified to include a link to the assets view.

---
+++ moocng/assets/templatetags/asset_tags.py
@@ -0,0 +1,61 @@
+# Copyright 2013 Rooter Analysis S.L.
+# Copyright (c) 2013 Grupo Opentia
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from django import template
+
+from moocng.mongodb import get_db
+from moocng.assets.utils import course_has_assets
+from moocng.assets.models import Asset
+
+register = template.Library()
+
+
+class IfHasAssetsNode(template.Node):
+
+    def __init__(self, course, nodelist_true, nodelist_false):
+        self.course = template.Variable(course)
+        self.nodelist_true = nodelist_true
+        self.nodelist_false = nodelist_false
+
+    def __repr__(self):
+        return "<IfHasAssetsNode>"
+
+    def render(self, context):
+        course = self.course.resolve(context)
+        if course_has_assets(course):
+            return self.nodelist_true.render(context)
+        else:
+            return self.nodelist_false.render(context)
+
+
+@register.tag
+def if_has_assets(parser, token):
+    bits = token.contents.split()
+
+    try:
+        course = bits[1]
+    except IndexError:
+        raise template.TemplateSyntaxError('%r tag requires a single argument'
+                                           % bits[0])
+
+    nodelist_true = parser.parse(('else', 'endif_has_assets'))
+    token = parser.next_token()
+    if token.contents == 'else':
+        nodelist_false = parser.parse(('endif_has_assets', ))
+        parser.delete_first_token()
+    else:
+        nodelist_false = template.NodeList()
+
+    return IfHasAssetsNode(course, nodelist_true, nodelist_false)
---
+++ moocng/assets/utils.py
@@ -0,0 +1,36 @@
+# Copyright 2013 Rooter Analysis S.L.
+# Copyright (c) 2013 Grupo Opentia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from datetime import datetime
+
+from django.db import IntegrityError
+
+from moocng.mongodb import get_db
+from moocng.assets import cache
+from moocng.assets.models import Asset
+
+
+def course_get_assets(course):
+    return Asset.objects.filter(kq__unit__course__id=course.id)
+
+
+def course_has_assets(course):
+    result = cache.get_course_has_assets_from_cache(course)
+    if result is None:
+        assets = course_get_assets(course)
+        result = assets.count() > 0
+        cache.set_course_has_assets_in_cache(course, result)
+    return result
