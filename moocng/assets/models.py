06e1be642a67120597e88fe506abf26d5d05e731
José Millán Soto <jmillan@opentia.com>
2013-05-14 12:22:35 +0200

    Import ObjectDoesNotExist

--- moocng/assets/models.py
+++ moocng/assets/models.py
@@ -14,6 +14,7 @@
 
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.db.models import signals, Q
 from django.utils.translation import ugettext_lazy as _
