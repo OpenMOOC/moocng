f1e4dfa4c5ad97f22d86e30fd0ea4a651547594a
ahmontero <antonio.hinojo.montero@gmail.com>
2013-09-23 14:15:07 +0200

    Deleted commented code

--- moocng/externalapps/forms.py
+++ moocng/externalapps/forms.py
@@ -10,6 +10,7 @@ from moocng.forms import BootstrapMixin
 class ExternalAppForm(forms.ModelForm, BootstrapMixin):
 
     error_messages = {
+        'slug_edit': _("Once the slug has been set, you must contact an administrator to change it"),
         'status_in_progress': _("The external application creation task is being processed. Save is not allowed in that status."),
         'status_tampering': _("Oops! It might seem you have tried tampering the object status..."),
     }
@@ -21,20 +22,19 @@ class ExternalAppForm(forms.ModelForm, BootstrapMixin):
         fields = ('instance_type', 'app_name', 'slug', 'status', 'visibility',)
 
     def clean(self):
-        status = self.cleaned_data.get('status')
-        if status and self.instance.id:
-            status = int(self.cleaned_data.get('status'))
-            slug = self.cleaned_data.get('slug')
-            instance_type = self.cleaned_data.get('instance_type')
-            if slug and instance_type:
-                try:
-                    externalapp = ExternalApp.objects.get(slug=slug, instance_type=instance_type)
-                except ExternalApp.DoesNotExist:
-                    pass
-                else:
-                    if status == externalapp.status:
-                        if status == ExternalApp.IN_PROGRESS:
-                            raise ValidationError(self.error_messages['status_in_progress'])
-                    else:
-                        raise ValidationError(self.error_messages['status_tampering'])
+        cleaned_data = super(ExternalAppForm, self).clean()
+        slug = cleaned_data.get("slug")
+        status = int(self.cleaned_data.get('status'))
+
+        if self.instance.id:
+            externalapp = ExternalApp.objects.get(pk=self.instance.id)
+            if slug and externalapp.slug != slug:
+                raise ValidationError(self.error_messages['slug_edit'])
+
+            if status == externalapp.status:
+                if status == ExternalApp.IN_PROGRESS:
+                    raise ValidationError(self.error_messages['status_in_progress'])
+            else:
+                raise ValidationError(self.error_messages['status_tampering'])
+
         return self.cleaned_data
