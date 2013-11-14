016fc1e811fe46a0c760602c819d830d319ddf14
Pablo Martín <goinnn@gmail.com>
2013-11-14 13:22:26 +0100

    Add the first version to the functions that update the score in the mongo

--- moocng/api/tasks.py
+++ moocng/api/tasks.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from django.contrib.auth.models import User
+
 from celery import task
 
 from moocng.courses.models import KnowledgeQuantum
@@ -105,8 +107,115 @@ def update_stats(submitted, data):
         )
 
 
+def update_kq_score(db, kq, user, new_score_kq=None):
+    from moocng.courses.marks import calculate_kq_mark
+    if not new_score_kq:
+        new_score_kq, use_in_total = calculate_kq_mark(kq, user)
+        if not use_in_total:
+            return False
+    data_kq = {}
+    data_kq['user_id'] = user.pk
+    data_kq['course_id'] = kq.unit.course.pk
+    data_kq['unit_id'] = kq.unit.pk
+    data_kq['kq_id'] = kq.pk
+
+    scores_kq = db.get_collection('scores_kq')
+    score_kq_item = scores_kq.find_one(data_kq)
+    if score_kq_item:
+        updated_kq_score = new_score_kq == score_kq_item['score']
+        scores_kq.update(
+            data_kq,
+            {'score': new_score_kq},
+            safe=True
+        )
+    else:
+        updated_kq_score = True
+        data_kq['score'] = new_score_kq
+        scores_kq.insert(data_kq)
+    return updated_kq_score
+
+
+def update_unit_score(db, unit, user, new_score_unit=None):
+    from moocng.courses.marks import calculate_unit_mark
+    if not new_score_unit:
+        new_score_unit, score_normalized_unit, use_unit_in_total = calculate_unit_mark(unit, user)
+    data_unit = {}
+    data_unit['user_id'] = user.pk
+    data_unit['course_id'] = unit.course.pk
+    data_unit['unit_id'] = unit.pk
+
+    scores_unit = db.get_collection('scores_unit')
+    score_unit_item = scores_unit.find_one(data_unit)
+    if score_unit_item:
+        updated_unit_score = new_score_unit == score_unit_item['score']
+        scores_unit.update(
+            data_unit,
+            {'score': new_score_unit},
+            safe=True
+        )
+    else:
+        updated_unit_score = True
+        data_unit['score'] = new_score_unit
+        scores_unit.insert(data_unit)
+    return updated_unit_score
+
+
+def update_course_score(db, course, user, new_score_course=None):
+    from moocng.courses.marks import calculate_course_mark
+    if not new_score_course:
+        new_score_course, units_info = calculate_course_mark(course, user)
+    data_course = {}
+    data_course['user_id'] = user.pk
+    data_course['course_id'] = course.pk
+
+    scores_course = db.get_collection('scores_course')
+    score_course_item = scores_course.find_one(data_course)
+    if score_course_item:
+        updated_course_score = new_score_course == score_course_item['score']
+        scores_course.update(
+            data_course,
+            {'score': new_score_course},
+            safe=True
+        )
+    else:
+        updated_course_score = True
+        data_course['score'] = new_score_course
+        scores_course.insert(data_course)
+    return updated_course_score
+
+
+def update_score(submitted, data):
+    from courses.marks import calculate_kq_mark, calculate_unit_mark, calculate_course_mark
+    updated_kq_score = updated_unit_score = updated_course_score = False
+    kq = KnowledgeQuantum.objects.get(pk=submitted['kq_id'])
+    user = User.objects.get(pk=submitted['user_id'])
+    score_kq, use_kq_in_total = calculate_kq_mark(kq, user)
+    score_unit, score_normalized_unit, use_unit_in_total = calculate_unit_mark(kq.unit, user)
+    score_course, units_info = calculate_course_mark(kq.unit.course, user)
+    if not use_kq_in_total:
+        return (updated_kq_score, updated_unit_score, updated_course_score)
+
+    db = get_db()
+
+    # KQ
+    updated_kq_score = update_kq_score(db, kq, user, score_kq)
+
+    # UNIT
+    if not updated_kq_score or not use_unit_in_total:
+        return (updated_kq_score, updated_unit_score, updated_course_score)
+
+    updated_unit_score = update_unit_score(db, kq.unit, user, score_unit)
+
+    # COURSE
+    if not updated_unit_score:
+        return (updated_kq_score, updated_unit_score, updated_course_score)
+    updated_course_score = update_course_score(db, kq.unit.course, user, score_course)
+    return (updated_kq_score, updated_unit_score, updated_course_score)
+
+
 @task
 def on_answer_created_task(answer_created):
+    update_score(answer_created, {'submitted': 1})
     update_stats(answer_created, {'submitted': 1})
 
 
