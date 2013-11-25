bef98472522f7dfa8730565dd0e61f9d96232f3c
Pablo Martín <goinnn@gmail.com>
2013-11-25 13:42:58 +0100

    Update the free enrollment view, this is for the changes of the model (CourseStudent)

--- moocng/enrollment/views.py
+++ moocng/enrollment/views.py
@@ -12,8 +12,14 @@ from moocng.enrollment.idp import enroll_course_at_idp
 def free_enrollment(request, course_slug):
     course = get_object_or_404(Course, slug=course_slug)
     if request.method == 'POST':
-        course.students.add(request.user)
-        course.save()
+        user = request.user
+        old_course_status = 'f'
+        if course.created_from:
+            if course.created_from.students.filter(pk=user.pk):
+                old_course_status = 'n'
+        course.students.through.objects.create(student=user,
+                                               course=course,
+                                               old_course_status=old_course_status)
         if getattr(settings, 'FREE_ENROLLMENT_CONSISTENT', False):
             enroll_course_at_idp(request.user, course)
         success(request,
@@ -27,8 +33,9 @@ def free_enrollment(request, course_slug):
 def free_unenrollment(request, course_slug):
     course = get_object_or_404(Course, slug=course_slug)
     if request.method == 'POST':
-        course.students.remove(request.user)
-        course.save()
+        user = request.user
+        course.students.through.objects.get(student=user,
+                                            course=course).delete()
         success(request,
                 _(u'You have successfully unenroll in the course %(course)s')
                 % {'course': unicode(course)})
